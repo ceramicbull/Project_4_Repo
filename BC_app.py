52d3d451ce34b77d78a8f3704c193616841f85a0
Ben Chilcoat <ceramicbull@gmail.com>
2023-08-03 18:13:09 -0700

    functional app code finished

--- BC_app.py
+++ BC_app.py
@@ -90,55 +90,69 @@ def predict():
     #obtain all form values and convert to pandas df
     data=request.form
     print(request.form)
-    print(request.form.values())
     #convert the "ImmutableMultiDict" into a standard dict:
     input_dict={}
     #get numerical values:
     for key in num_keys:
         input_dict[key]=data.getlist(key)[0]
+     #convert to pandas df
+    print(input_dict)
+    input_df=pd.DataFrame(input_dict, index=[0])
+    #add categorical values to DataFrame
+    for key in career_keys:
+        input_df[key]=[0]
+    for key in bmi_keys:
+        input_df[key]=[0]
+    for key in gen_keys:
+        input_df[key]=[0]
+    print(list(input_df.columns))
     #determine career value:
     car=data.getlist('career')[0]
     print(car)
     #make selected career 1, all others 0:
-    for c in career_keys:
-        if c==car:
-            input_dict[c]=1
-    else: input_dict[c]=0
+    # for c in career_keys:
+    #     if c==car:
+    #         input_dict[c]=1
+    # else: input_dict[c]=0
     #determine bmi value:
     bmi_=data.getlist('bmi')[0]
     print(bmi_)
     #make selected bmi value 1, all others 0:
-    for b in bmi_keys:
-        if b==bmi_:
-            input_dict[b]=1
-    else: input_dict[b]=0
+    # for b in bmi_keys:
+    #     if b==bmi_:
+    #         input_dict[b]=1
+    # else: input_dict[b]=0
     #determine gender value:
     gen_=data.getlist('gender')[0]
     print(gen_)
     #make selected gender value 1, all others 0:
-    for g in gen_keys:
-        if g==gen_:
-            input_dict[g]=1
-    else: input_dict[g]=0
-    #convert to pandas df
-    print(input_dict)
-    input_df=pd.DataFrame(input_dict, index=[0])
+    # for g in gen_keys:
+    #     if g==gen_:
+    #         input_dict[g]=1
+    # else: input_dict[g]=0
+    #change selected categorical values to 1:
+    cat_list=[car,bmi_,gen_]
+    for cat in cat_list:
+        input_df.iloc[0, input_df.columns.get_loc(cat)] = 1
     #ensure DataFrame is in correct order:
     input_df=input_df[keys]
+    print(list(input_df.columns))
+    print(list(input_df.iloc[0,:]))
     #scale input DataFrame:
     input_=input_scaler.transform(input_df)
     #predict the price given the values inputted by user
     prediction = model.predict(input_)
+    print(prediction)
    
     
     #If the output is negative, the values entered are unreasonable to the context of the application
     #If the output is greater than 0, return prediction
-    if prediction < 0.5 & prediction>0:
-        return render_template('index.html', prediction_text = "It is unlikely that you have a Sleeping Disorder.")
-    elif prediction >= 0.5:
-        return render_template('index.html', prediction_text = "It is likely that you have a Sleeping Disorder.")   
+    if (prediction < 0.5) & (prediction >= 0):
+        return render_template('BC_index.html', prediction_text = "It is unlikely that you have a Sleeping Disorder.")
+    elif (prediction >= 0.5) & (prediction < 1):
+        return render_template('BC_index.html', prediction_text = "It is likely that you have a Sleeping Disorder.")   
     else: 
-        return render_template('index.html', prediction_text = "Uh oh. Something went wrong.")   
+        return render_template('BC_index.html', prediction_text = "Uh oh. Something went wrong.")   
 #Run app
 
 if __name__ == '__main__':
